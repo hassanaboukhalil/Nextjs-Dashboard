1fc217ee588c89d57b7f74cec7f89b24fe881d89
hassan <hassanak509@gmail.com>
2025-08-23 12:05:43 +0300

    Add logging for search term in Search component; removed redundant log.

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -9,6 +9,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const { replace } = useRouter();
 
   const handleSearch = (term: string) => {
+    console.log(`Searching... ${term}`);
+
     const params = new URLSearchParams(searchParams);
 
     if (term) {
@@ -18,8 +20,6 @@ export default function Search({ placeholder }: { placeholder: string }) {
     }
 
     replace(`${pathname}?${params.toString()}`);
-
-    console.log(term);
   };
 
   return (
