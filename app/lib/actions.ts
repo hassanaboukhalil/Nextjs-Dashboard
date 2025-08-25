cbf6a60a44bd9449313a40de0b510e9ba2551c46
hassan <hassanak509@gmail.com>
2025-08-25 10:28:27 +0300

    Add updateInvoice and deleteInvoice actions; implement redirects after modifications

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,6 +6,7 @@
 import { z } from "zod";
 import postgres from "postgres";
 import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
@@ -18,6 +19,7 @@ const FormSchema = z.object({
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
   //   const rawFormData = {
@@ -42,5 +44,32 @@ export async function createInvoice(formData: FormData) {
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
 
-  revalidatePath("/dashboard/invoices"); // revalidate the invoices page to show the new invoice, because the invoices page(or any other page) is cached by default
+  revalidatePath("/dashboard/invoices"); // Calling revalidatePath to clear the client cache and make a new server request. Revalidate the invoices page to show the new invoice, because the invoices page(or any other page) maybe cached
+  redirect("/dashboard/invoices"); // redirect to the invoices page
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
+}
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath("/dashboard/invoices");
+
+  // Since this action is being called in the /dashboard/invoices path, you don't need to call redirect. Calling revalidatePath will trigger a new server request and re-render the table.
 }
