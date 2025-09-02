bbadbe455d007ce3370995d6371e866b25ec4d90
hassan <hassanak509@gmail.com>
2025-09-02 07:28:19 +0300

    Implement error handling and state management in edit-invoice form

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -13,8 +13,12 @@ import { createInvoice, State } from "@/app/lib/actions";
 import { useActionState } from "react";
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
-  const initialState: State = { message: null, errors: {} };
+  const initialState: State = {
+    message: null,
+    errors: {},
+  };
   const [state, formAction] = useActionState(createInvoice, initialState);
+
   return (
     <form action={formAction}>
       <div
